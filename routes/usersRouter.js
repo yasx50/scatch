5ed53163346f49473eac3190b6eaf890e9727a50
yasx50 <yasxcode@gmail.com>
2024-09-12 22:40:39 +0530

    created three routes for owner products users

---
+++ routes/usersRouter.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    console.log('this is users routers');
+    
+
+})
+
+
+module.exports = router;
